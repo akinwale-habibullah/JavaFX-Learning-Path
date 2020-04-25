08926032f2eedcce211000b3af003c6c076c7348
Akinwale Habib <akinwalehabib@hotmail.com>
2020-04-25 11:45:28 +0100

    ch: learn basic UI design using JavaFX scene controls and layout

--- src/com/akinwalehabib/doit/doit.java
+++ src/com/akinwalehabib/doit/doit.java
@@ -6,10 +6,18 @@
 package com.akinwalehabib.doit;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.Scene;
 
 /**
@@ -21,31 +29,53 @@ public class doit extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         GridPane grid = new GridPane();
-        grid.setGridLinesVisible(true);
-        grid.setHgap(10);
+        grid.setMinWidth(600);
+        grid.setMinHeight(400);
+        
+        grid.setHgap(5);
         grid.setVgap(10);
+        grid.setGridLinesVisible(false);
         
-        Button b1 = new Button("First Button");
-        Button b2 = new Button("Long Second Button");
-        Button b3 = new Button("Third Button");
-        Button b4 = new Button("Fourth Button");
+        TableColumn column1 = new TableColumn("Priority");
+        TableColumn column2 = new TableColumn("Description");
+        TableColumn column3 = new TableColumn("Progress");
         
-        GridPane.setConstraints(b1, 1, 1);
-        GridPane.setConstraints(b2, 2, 1);
-        GridPane.setConstraints(b3, 1, 2);
-        GridPane.setConstraints(b4, 2, 2);
+        TableView table = new TableView();
+        table.setMinHeight(300);
+        table.setMinWidth(550);
+        table.getColumns().addAll(column1, column2, column3);
+        GridPane.setConstraints(table, 1, 1, 3, 1);
         
-        BorderPane borderpane = new BorderPane();
-        borderpane.setTop(new Button("top"));
-        borderpane.setBottom(new Button("bottom"));
-        borderpane.setLeft(new Button("left"));
-        borderpane.setCenter(new Button("Center"));
-        borderpane.setRight(new Button("left"));
-        GridPane.setConstraints(borderpane, 1, 3, 4, 2);
+        TextField taskName = new TextField();
+        taskName.setPromptText("Enter task name");
+        taskName.setText("Default text");
+        taskName.setMinWidth(300);
+        GridPane.setConstraints(taskName, 2, 2);
         
-        grid.getChildren().addAll(b1, b2, b3, b4, borderpane);
+        ComboBox priority = new ComboBox();
+        priority.getItems().addAll("High", "Low", "Medium");
+        priority.setPromptText("Enter Priority");
+        GridPane.setConstraints(priority, 1, 2);
         
-        Integer width = new Integer(300);
+        Button addButton = new Button("Add");
+        addButton.setMinWidth(100);
+        GridPane.setConstraints(addButton, 3, 2);
+        
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setMinWidth(100);
+        GridPane.setConstraints(cancelButton, 3, 3);
+        
+        HBox progressArea = new HBox();
+        progressArea.getChildren().addAll(new Label("Progress"),
+                                          new Spinner<Integer>(0, 100, 0),
+                                          new CheckBox("Completed"));
+        progressArea.setAlignment(Pos.CENTER_RIGHT);
+        progressArea.setSpacing(10);
+        GridPane.setConstraints(progressArea, 1, 3, 2, 1);
+        
+        grid.getChildren().addAll(table, taskName, priority, addButton, cancelButton, progressArea);
+        
+        Integer width = new Integer(600);
         Integer height = new Integer(400);
         Scene scene = new Scene(grid, width, height);
         /**
