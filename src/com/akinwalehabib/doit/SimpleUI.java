c9ba87e8d838508fe0c134b231d6f2ce9a942cec
Akinwale Habib <akinwalehabib@hotmail.com>
2020-04-25 13:50:55 +0100

    ch: learn fxml, controllers and reference elements in main application

---
+++ src/com/akinwalehabib/doit/SimpleUI.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.akinwalehabib.doit;
+
+import java.lang.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+import com.akinwalehabib.doit.SimpleUIController;
+
+/**
+ *
+ * @author Habib
+ */
+public class SimpleUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Access textfield in our application class
+        // we need to get a reference to our controller object
+        // for that we need to create a FXMLLoader object
+        FXMLLoader loader = new FXMLLoader(SimpleUI.class.getResource("simpleui.fxml"));
+        HBox box = loader.load();
+        SimpleUIController controller = loader.getController();
+        
+        System.out.println(controller.textfield.getText());
+        
+        Scene scene = new Scene(box);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args){
+        Application.launch(args);
+    }
+}
---
+++ src/com/akinwalehabib/doit/SimpleUIController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.akinwalehabib.doit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+/**
+ *
+ * @author Habib
+ */
+public class SimpleUIController {
+    
+    @FXML
+    public TextField textfield;
+    
+    public void initialize() {
+        System.out.println("SimpleUIController is initialized");
+        
+        textfield.setText("This is the textfield's text set in SimpleUIController");
+    }
+}
